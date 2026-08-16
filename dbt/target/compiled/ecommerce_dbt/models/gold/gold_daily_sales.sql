228e509d0c59a905ac78e36a8fd4a305f86a031c
faizal4757 <faizalahmedmotog3@gmail.com>
2026-08-16 14:36:21 +0530

    ignore dbt generated files

--- dbt/target/compiled/ecommerce_dbt/models/gold/gold_daily_sales.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    processing_date,
-    COUNT(DISTINCT order_id) AS order_count,
-    SUM(quantity) AS units_sold,
-    SUM(quantity * unit_price) AS revenue
-FROM `workspace`.`ecommerce`.`silver_orders`
-GROUP BY processing_date
